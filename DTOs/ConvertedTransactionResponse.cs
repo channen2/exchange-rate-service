0f6b67d7a3f61bdc6a544ffeb1ede9637a8902c7
Channen Chong <channen2@gmail.com>
2026-05-22 22:09:06 -0500

    feature: configure swagger ui

---
+++ DTOs/ConvertedTransactionResponse.cs
@@ -0,0 +1,19 @@
+namespace ExchangeRateService.DTOs
+{
+    public class ConvertedTransactionResponse
+    {
+        public Guid Id { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime TransactionDate { get; set; }
+
+        public decimal OriginalPurchaseAmountUsd { get; set; }
+
+        public decimal ExchangeRate { get; set; }
+
+        public string TargetCurrency { get; set; } = string.Empty;
+
+        public decimal ConvertedAmount { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,11 +4,10 @@ using Microsoft.EntityFrameworkCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<TransactionService>();
 
@@ -21,11 +20,14 @@ WebApplication app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    _ = app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.MapControllers();
-
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
